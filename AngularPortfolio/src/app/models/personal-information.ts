a0149eb30750d7a040478ee316ac08ccff9aafdb
mdmiroz <mdmirozjhpi666@gmail.com>
2023-08-30 23:12:38 +0600

    Create class for personal-information

---
+++ AngularPortfolio/src/app/models/personal-information.ts
@@ -0,0 +1,15 @@
+export class PersonalInformation {
+    id:number|undefined;
+    firstName: string|undefined;
+    lastName : string |undefined;
+    email : string |undefined;
+    phone : string |undefined;
+    address : string |undefined;
+    city : string |undefined;
+    state : string |undefined;
+    zipCode : string |undefined;
+    country : string |undefined;
+    profileSummary : string |undefined;
+    linkedinProfile : string |undefined;
+    githubProfile : string |undefined;
+}
