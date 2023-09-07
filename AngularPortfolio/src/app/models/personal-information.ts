137cacb38581d38843d5f7c5defa011ef56b9591
S71-GAZi <sorifullahpavelgazi@gmail.com>
2023-09-07 17:59:21 +0600

    update ui and list of data

--- AngularPortfolio/src/app/models/personal-information.ts
+++ AngularPortfolio/src/app/models/personal-information.ts
@@ -1,15 +1,26 @@
 export class PersonalInformation {
-    id:number|undefined;
-    firstName: string|undefined;
-    lastName : string |undefined;
-    email : string |undefined;
-    phone : string |undefined;
-    address : string |undefined;
+    id:number|undefined = 1;
+    firstName: string|undefined = "Sorifullah";
+    lastName : string |undefined = "Pavel";
+    email : string |undefined = "sorifullahpavelgazi@gmail.com";
+    phone : string |undefined = "01760802604";
+    address : string |undefined = "Dhaka, Bangladesh";
     city : string |undefined;
     state : string |undefined;
     zipCode : string |undefined;
     country : string |undefined;
-    profileSummary : string |undefined;
+    profileSummary : string |undefined = 'Full Stack Software Engineer working for PerkyRabbit Corporation Ltd. Dhaka, Bangladesh.';
+    currentDesignation : string |undefined = "Software Engineer";
     linkedinProfile : string |undefined;
     githubProfile : string |undefined;
+    professionStartDate:Date | undefined = new Date('2022-07-24');
+
 }
+
+
+/* const date1 = new Date('2024-09-24');
+const date2 = new Date('2024-09-25');
+
+const msBetweenDates = date2.getTime() - date1.getTime();
+
+console.log(msBetweenDates); */
--- AngularPortfolio/src/app/pages/main-page/main-page.component.ts
+++ AngularPortfolio/src/app/pages/main-page/main-page.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import {PersonalInformation} from '../../models/personal-information';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
+
+
 export class MainPageComponent {
+ person:PersonalInformation =new PersonalInformation;
 name:string='SORIFULLAH PAVEL';
 shortDesctiption:string = 'Full Stack Software Engineer working for PerkyRabbit Corporation Ltd. Dhaka, Bangladesh.';
 designation :string ="Software Engineer";
